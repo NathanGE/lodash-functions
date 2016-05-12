a916084bfb1478863743b6f2356ca30bee3068ef
Nathan Elliott <nathangelliott@gmail.com>
2016-05-12 16:06:26 +0000

    Initial commit

---
+++ join_method.js
@@ -0,0 +1,21 @@
+"use strict"
+
+var _ = {
+  //Implements:
+  // https://lodash.com/docs#join
+  join: (array, separator) => {
+    var result = '';
+    var separator = typeof separator !== 'undefined' ? separator : ', ';
+    array.forEach(function(value){
+      if (value === array[array.length - 1]){
+        result += value;
+      } else {
+        result += value + separator;
+      }
+    });
+    return result;
+  }
+}
+
+const value = _.join(["hello", "goodbye"], ", ");
+console.log(value);
