e2887f8d4b5d86a7d184c2d4b02911c7cbdc299b
NathanGE <nathangelliott@gmail.com>
2016-05-14 11:47:39 -0400

    Updated to es6 syntax and added trailing newline

--- map_method.js
+++ map_method.js
@@ -8,18 +8,16 @@ var _ = {
     if(callback === undefined){
       return array;
     }
-    array.forEach(function(item){
+    for(let item of array) {
       result.push(callback(item));
-    });
+    }
     return result;
   }
 }
 
-// Define a method that multiplies the input by 10
 function multiplyBy10(value) {
   return 10 * value;
 }
 const value = _.map([1, 2, 3], multiplyBy10);
 
-// value => [10, 20, 30]
 console.log(value);
